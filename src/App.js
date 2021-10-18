be4b09e8235426bdeb010747f1a0a12c385309a3
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-10-18 21:57:31 +0600

    Font Installed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>THis is header</h1>
+      <h1 className="font-body fw-bold">THis is header</h1>
     </div>
   );
 }
