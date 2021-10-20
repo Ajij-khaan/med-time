26de1549311f3c41ea1132e6b461b0626d7f7a85
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-10-20 18:25:25 +0600

    safety

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const ServiceDetails = () => {
 
     const { serviceId } = useParams();
 
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('fakeData.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    console.log(services);
+
     return (
         <div>
             <p>THis si service {serviceId}</p>
+            {
+                services.map(service => console.log(service))
+            }
         </div>
     );
 };
